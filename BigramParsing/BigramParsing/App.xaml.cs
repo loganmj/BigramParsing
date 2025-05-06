0cf9f643298c1fe3bb0fba42b1fd9f97a9dc73e4
Michael <michaellogan7860@gmail.com>
2025-05-06 10:54:16 -0500

    Added mainwindow viewmodel and configured app.xaml.cs to construct a viewmodel on start.

--- BigramParsing/BigramParsing/App.xaml.cs
+++ BigramParsing/BigramParsing/App.xaml.cs
@@ -1,14 +1,27 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace BigramParsing
+namespace BigramParser
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        #region Event Handlers
+
+        /// <summary>
+        /// Performs necessary code at startup.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindowViewModel = new MainWindowViewModel();
+            var mainWindow = new MainWindow(mainWindowViewModel);
+            mainWindow.Show();
+        }
+
+        #endregion
     }
 
 }
