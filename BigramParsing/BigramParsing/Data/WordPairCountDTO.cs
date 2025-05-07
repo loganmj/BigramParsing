e0fff31fe906cbdadc9dd069dbe234e723f38744
Michael <michaellogan7860@gmail.com>
2025-05-07 07:51:59 -0500

    Removed the CompareTo() method from WordPairCountDTO, as it is no longer needed.

--- BigramParsing/BigramParsing/Data/WordPairCountDTO.cs
+++ BigramParsing/BigramParsing/Data/WordPairCountDTO.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// A data transfer object for word pairs and their counts.
     /// </summary>
-    public class WordPairCountDTO : IComparable<WordPairCountDTO>
+    public class WordPairCountDTO
     {
         #region Properties
 
@@ -31,17 +31,6 @@
         {
             return $"\"{Word1} {Word2}\": {Count}";
         }
-
-
-        /// <summary>
-        /// Compares this instance with another WordPairCountDTO based on Count.
-        /// </summary>
-        public int CompareTo(WordPairCountDTO? other)
-        {
-            if (other == null) return 1;
-            return Count.CompareTo(other.Count);
-        }
-
 
         #endregion
     }
